387d31607df90001595ce501ac4a9ac105459697
Jack Chaney <jbc282@cornell.edu>
2025-09-07 12:57:52 -0400

    i ate a bear

--- Audio/c_Audio_Beep_Synthesis_Single_Core/beep_beep.c
+++ Audio/c_Audio_Beep_Synthesis_Single_Core/beep_beep.c
@@ -12,6 +12,20 @@
     3.3v (pin 36) -> VCC on DAC 
     GND (pin 3)  -> GND on DAC 
 
+    KEYPAD CONNECTIONS
+    - GPIO 9   -->  330 ohms  --> Pin 1 (button row 1)
+    - GPIO 10  -->  330 ohms  --> Pin 2 (button row 2)
+    - GPIO 11  -->  330 ohms  --> Pin 3 (button row 3)
+    - GPIO 12  -->  330 ohms  --> Pin 4 (button row 4)
+    - GPIO 13  -->     Pin 5 (button col 1)
+    - GPIO 14  -->     Pin 6 (button col 2)
+    - GPIO 15  -->     Pin 7 (button col 3)
+ 
+    SERIAL CONNECTIONS
+    - GPIO 0        -->     UART RX (white)
+    - GPIO 1        -->     UART TX (green)
+    - RP2040 GND    -->     UART GND
+
  */
 
 // Include necessary libraries
@@ -21,12 +35,39 @@
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 #include "hardware/sync.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/multicore.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/clocks.h"
+
+// VGA graphics library
+#include "vga16_graphics_v2.h"
+#include "pt_cornell_rp2040_v1_4.h"
+
 // Include protothreads
 #include "pt_cornell_rp2040_v1_4.h"
 
 // Low-level alarm infrastructure we'll be using
 #define ALARM_NUM 0
 #define ALARM_IRQ TIMER_IRQ_0
+
+// Keypad pin configurations
+#define BASE_KEYPAD_PIN 9
+#define KEYROWS         4
+#define NUMKEYS         12
+
+#define LED             25
+
+// Constants for keypad
+unsigned int keycodes[12] = {   0x28, 0x11, 0x21, 0x41, 0x12,
+                                0x22, 0x42, 0x14, 0x24, 0x44,
+                                0x18, 0x48} ;
+unsigned int scancodes[4] = {   0x01, 0x02, 0x04, 0x08} ;
+unsigned int button = 0x70 ;
+char keytext[40];
+int prev_key = 0;
 
 // Macros for fixed-point arithmetic (faster than floating point)
 typedef signed int fix15 ;
@@ -157,6 +198,7 @@ static void alarm_irq(void) {
 }
 
 
+// blinking light thread
 // This thread runs on core 0
 static PT_THREAD (protothread_core_0(struct pt *pt))
 {
@@ -174,8 +216,55 @@ static PT_THREAD (protothread_core_0(struct pt *pt))
     PT_END(pt) ;
 }
 
+// Keyboard thread
+// This thread runs on core 0
+static PT_THREAD (protothread_core_0(struct pt *pt))
+{
+    // Indicate thread beginning
+    PT_BEGIN(pt) ;
 
+    // Some variables
+    static int i ;
+    static uint32_t keypad ;
 
+    while(1) {
+
+        gpio_put(LED, !gpio_get(LED)) ;
+
+        // Scan the keypad!
+        for (i=0; i<KEYROWS; i++) {
+            // Set a row high
+            gpio_put_masked((0xF << BASE_KEYPAD_PIN),
+                            (scancodes[i] << BASE_KEYPAD_PIN)) ;
+            // Small delay required
+            sleep_us(1) ; 
+            // Read the keycode
+            keypad = ((gpio_get_all() >> BASE_KEYPAD_PIN) & 0x7F) ;
+            // Break if button(s) are pressed
+            if (keypad & button) break ;
+        }
+        // If we found a button . . .
+        if (keypad & button) {
+            // Look for a valid keycode.
+            for (i=0; i<NUMKEYS; i++) {
+                if (keypad == keycodes[i]) break ;
+            }
+            // If we don't find one, report invalid keycode
+            if (i==NUMKEYS) (i = -1) ;
+        }
+        // Otherwise, indicate invalid/non-pressed buttons
+        else (i=-1) ;
+
+        // Print key to terminal
+        printf("\n%d", i) ;
+
+        PT_YIELD_usec(30000) ;
+    }
+    // Indicate thread end
+    PT_END(pt) ;
+}
+
+// dds main
 // Core 0 entry point
 int main() {
     // Initialize stdio/uart (printf won't work unless you do this!)
@@ -227,6 +316,40 @@ int main() {
     irq_set_enabled(ALARM_IRQ, true) ;
     // Write the lower 32 bits of the target time to the alarm register, arming it.
     timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
+
+    // Add core 0 threads
+    pt_add_thread(protothread_core_0) ;
+
+    // Start scheduling core 0 threads
+    pt_schedule_start ;
+
+}
+
+// keypad main
+int main() {
+
+    // Overclock
+    set_sys_clock_khz(150000, true) ;
+
+    // Initialize stdio
+    stdio_init_all();
+
+    // Map LED to GPIO port, make it low
+    gpio_init(LED) ;
+    gpio_set_dir(LED, GPIO_OUT) ;
+    gpio_put(LED, 0) ;
+
+    ////////////////// KEYPAD INITS ///////////////////////
+    // Initialize the keypad GPIO's
+    gpio_init_mask((0x7F << BASE_KEYPAD_PIN)) ;
+    // Set row-pins to output
+    gpio_set_dir_out_masked((0xF << BASE_KEYPAD_PIN)) ;
+    // Set all output pins to low
+    gpio_put_masked((0xF << BASE_KEYPAD_PIN), (0x0 << BASE_KEYPAD_PIN)) ;
+    // Turn on pulldown resistors for column pins (on by default)
+    gpio_pull_down((BASE_KEYPAD_PIN + 4)) ;
+    gpio_pull_down((BASE_KEYPAD_PIN + 5)) ;
+    gpio_pull_down((BASE_KEYPAD_PIN + 6)) ;
 
     // Add core 0 threads
     pt_add_thread(protothread_core_0) ;
